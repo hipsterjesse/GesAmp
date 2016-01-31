f4b37ad2d849d505369c19321eb31e766b9741a8
Michael <jesenkom@edu.htl-villach.at>
2016-01-31 16:16:43 +0100

    Add Class Song

---
+++ app/src/main/java/studio6ty9/gesamp/Song.java
@@ -0,0 +1,30 @@
+package studio6ty9.gesamp;
+
+/**
+ * Created by Michael on 31.01.2016.
+ */
+public class Song {
+    private String SongTitle;
+    private String SongPath;
+
+    public Song(String songTitle, String songPath) {
+        SongTitle = songTitle;
+        SongPath = songPath;
+    }
+
+    public String getSongTitle() {
+        return SongTitle;
+    }
+
+    public void setSongTitle(String songTitle) {
+        SongTitle = songTitle;
+    }
+
+    public String getSongPath() {
+        return SongPath;
+    }
+
+    public void setSongPath(String songPath) {
+        SongPath = songPath;
+    }
+}
