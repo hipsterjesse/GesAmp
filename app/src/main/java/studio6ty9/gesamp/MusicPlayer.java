415953a266e8080902fad8d951091c2ceb714d9e
kerschbd <dominik.kerschbaumer98@gmail.com>
2016-02-01 20:30:00 +0100

    versuch song löschen

--- app/src/main/java/studio6ty9/gesamp/MusicPlayer.java
+++ app/src/main/java/studio6ty9/gesamp/MusicPlayer.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -12,6 +13,7 @@ public class MusicPlayer extends AppCompatActivity {
     private int songId;
     private Song currentSong;
     private MediaPlayer player;
+    private TextView textViewTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,13 @@ public class MusicPlayer extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         songId = extras.getInt("songId");
         currentSong = SongList.getSongById(songId);
+
+        textViewTitle = (TextView) findViewById(R.id.textViewTitle);
+        textViewTitle.append(currentSong.getSongTitle());
+
         player = MediaPlayer.create(this, Uri.fromFile(new File(currentSong.getSongPath())));
         player.start();
+
+
     }
 }
