34fbd97211ab53e88097e2a5e9e260e12fa3b135
Kingcuber <muxim97@gmx.at>
2016-02-02 00:12:07 +0100

    LOADING SCRREEEEN

---
+++ app/src/main/java/studio6ty9/gesamp/SplashScreen.java
@@ -0,0 +1,38 @@
+package studio6ty9.gesamp;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MusicList.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+
+}
